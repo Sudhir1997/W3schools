7e1bddf5be68e8641ea583597f2d3f2af3b6e0c6
sudhir dhawan <sudhawan2@gmail.com>
2020-09-16 20:02:41 +0000

    new Compoent

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Nav from './components/First.jsx';
+
 
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Nav />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
